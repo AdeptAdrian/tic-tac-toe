bd652c16322c2399f298ea096f0d0ef7804c5dc6
Adrian <adrian.01@list.ru>
2024-10-13 20:41:13 -0500

    Create App.cs

---
+++ src/App.cs
@@ -0,0 +1,25 @@
+.square {
+	background: #ffffff;
+	border: 2px;
+	border-style: solid;
+	border-color: purple;
+	border-radius: 10px;
+	height: 50px;
+	width: 50px;
+	font-size: 30px;
+	margin-right: -2px;
+	margin-top: -2px;
+	padding: 0;
+	text-align: center;
+	line-height: 34px;
+	float: left;
+}
+
+.row {
+	display: table;
+}
+
+.status {
+	padding-bottom: 10px;
+	font-size: 30px;
+}
